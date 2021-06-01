eb4cfa8841de770f48cb8970b96d295ca848d651
Elian Avalos <Elianemanuelgit@gmail.com>
2021-06-01 02:15:05 -0400

    agregué el boton de refres para que el usuario elija el tamaño del grid y terminé.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
-const div = [];
+const refresh = document.querySelector("#refresh--button");
+let div = [];
 const Colors = ["red", "blue", "yellow", "violet", "pink",
             "skyblue", "brown", "blueviolet", "crimson", "lime"];
 class Pixel
@@ -21,7 +22,16 @@ class Pixel
         let random = Math.floor(Math.random() * this.colors.length + 1);
         let returnColor = this.colors[random];
         this.colors.splice(random, 1);
-        return returnColor;
+
+        if (this.colorCount === 10)
+        {
+            return "black";
+        }
+        else
+        {
+            this.colorCount++;
+            return returnColor;
+        }
     }
 };
 
@@ -39,14 +49,41 @@ function createDivs(divs)
 }
 
 
-
-
-createDivs(16)
-
-div.forEach((pixel) =>
-{
-    pixel.divElement.addEventListener("mouseover", () => 
+refresh.addEventListener("click", () => {
+    let pixeles = parseInt(prompt("elige el tamaño de tu cuadricula (menor 100)"));
+    while(pixeles >= 100 || pixeles < 0)
     {
-        pixel.changeDivsColor()
+        pixeles =  parseInt(prompt("elige el tamaño de tu cuadricula (menor 100)"));
+    }
+
+    div.forEach((pixel) =>
+    {
+        pixel.divElement.remove()
     })
+    div = [];
+
+    createDivs(pixeles * pixeles);
+    changeGridValues(pixeles);
+    addEvent();
 })
+
+function addEvent()
+{
+    div.forEach((pixel) =>
+    {
+        pixel.divElement.addEventListener("mouseover", () => 
+        {
+            pixel.changeDivsColor()
+        })
+    })
+}
+
+function changeGridValues(grid = 16)
+{
+    container.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
+
+}
+
+createDivs(16 * 16)
+addEvent()
